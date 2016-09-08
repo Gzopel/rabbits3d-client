5ca0ff516d9c5b4491e08e47ae3b18474b04986c
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-09 01:52:48 +0200

    Added ACTIONS and KEYS constants

---
+++ views/actions.js
@@ -0,0 +1,15 @@
+const ACTIONS = {
+  RESET: 'RESET',
+  IDLE: 'IDLE',
+  MOVE: {
+    UP: 'UP',
+    LEFT: 'LEFT',
+    DOWN: 'DOWN',
+    RIGHT: 'RIGHT',
+  },
+  CAMERA: {
+    ROTATE: 'ROTATE',
+  },
+};
+
+export default ACTIONS;
---
+++ views/keys.js
@@ -0,0 +1,9 @@
+const KEYS = {
+  I: 105,
+  J: 106,
+  K: 107,
+  L: 108,
+  ENTER: 13,
+};
+
+export default KEYS;
