a54eb5953b5c6370fe50b87acf531c621ec98a59
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-09 14:38:17 +0200

    Children components for game seen work now

--- views/components/Game.jsx
+++ views/components/Game.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import GameScene from './GameScene';
+import Map from './Map';
+import Character from './Character';
 
 const Game = () => {
   return (
-    <GameScene width={window.innerWidth} height={window.innerHeight} />
+    <GameScene width={window.innerWidth} height={window.innerHeight}>
+      <Map />
+      <Character />
+    </GameScene>
   );
 };
 
--- views/components/GameScene.jsx
+++ views/components/GameScene.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Map from './Map';
-import Character from './Character';
 import Camera from '../containers/Camera';
 
 const ReactTHREE = require('react-three');
@@ -8,13 +6,12 @@ const ReactTHREE = require('react-three');
 const Renderer = ReactTHREE.Renderer;
 const Scene = ReactTHREE.Scene;
 
-const GameScene = ({ width, height }) => {
+const GameScene = ({ width, height, children }) => {
   return (
     <Renderer width={width} height={height}>
       <Scene width={width} height={height} camera="maincamera">
         <Camera />
-        <Map />
-        <Character />
+        {children}
       </Scene>
     </Renderer>
   );
@@ -23,6 +20,7 @@ const GameScene = ({ width, height }) => {
 GameScene.propTypes = {
   width: React.PropTypes.number.isRequired,
   height: React.PropTypes.number.isRequired,
+  children: React.PropTypes.array.isRequired,
 };
 
 export default GameScene;
