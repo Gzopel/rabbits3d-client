3ffc30ccfd805da737c8e50b68b6e251727ffd70
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-09 05:22:38 +0200

    Add camera reset

--- views/reducers/Camera.js
+++ views/reducers/Camera.js
@@ -8,7 +8,7 @@ const InitialState = {
 
 const mapRotationToVector = (rotation) => {
   switch (rotation) {
-    // TODO: I know this aren't actions but it may come-handy afterwards
+    // TODO: I know these aren't actions but once cleaned up we should use them
     case ACTIONS.MOVE.UP:
       return new Three.Vector3(0, 1, 0);
     case ACTIONS.MOVE.DOWN:
@@ -28,6 +28,12 @@ const CameraReducer = (state = InitialState, action) => {
   switch (action.type) {
     case ACTIONS.CAMERA.ROTATE:
       // TODO: Make sure this is immutable
+      if (action.rotation === ACTIONS.RESET) {
+        return {
+          rotationVector: mapRotationToVector(action.rotation),
+        }
+      }
+
       return {
         rotationVector: state.rotationVector.add(mapRotationToVector(action.rotation)),
       };
