b6bfcbd86de16080a72678a2b014db317e9034c2
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-09 04:18:38 +0200

    Added root reducer and redux-devtools components

--- views/index.js
+++ views/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import Game from './components/Game';
-import CameraReducer from './reducers/Camera';
+import RootReducer from './reducers';
+import DevTools from './containers/DevTools';
 
-const store = createStore(CameraReducer);
+const store = createStore(
+  RootReducer,
+  compose(
+    DevTools.instrument()
+  )
+);
 
 render((
   <Provider store={store}>
-    <Router history={browserHistory} >
-      <Route path="/" component={Game} />
-    </Router>
+    <div>
+      <Router history={browserHistory} >
+        <Route path="/" component={Game} />
+      </Router>
+      <DevTools />
+    </div>
   </Provider>
 ),
 document.getElementById('app'));
---
+++ views/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import Camera from './Camera';
+
+const rootReducer = combineReducers({
+  Camera,
+});
+
+export default rootReducer;
