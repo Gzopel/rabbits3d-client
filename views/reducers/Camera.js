c89807621f90037bb86ac27ce17f9df8358aec63
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-09 14:30:46 +0200

    Cleaned up camera reducer

--- views/reducers/Camera.js
+++ views/reducers/Camera.js
@@ -6,36 +6,30 @@ const InitialState = {
   rotationVector: new Three.Vector3(0, 0, 0),
 };
 
-const mapRotationToVector = (rotation) => {
-  switch (rotation) {
-    // TODO: I know these aren't actions but once cleaned up we should use them
+const mapRotationVectorFromAction = (state, action) => {
+  // TODO: Add factory for Three Objects
+
+  const speed = action.speed || 1;
+  switch (action.rotation) {
     case ACTIONS.MOVE.UP:
-      return new Three.Vector3(0, 1, 0);
+      return state.rotationVector.add(new Three.Vector3(0, 1 * speed, 0));
     case ACTIONS.MOVE.DOWN:
-      return new Three.Vector3(0, -1, 0);
+      return state.rotationVector.add(new Three.Vector3(0, -1  * speed, 0));
     case ACTIONS.MOVE.LEFT:
-      return new Three.Vector3(-1, 0, 0);
+      return state.rotationVector.add(new Three.Vector3(-1  * speed, 0, 0));
     case ACTIONS.MOVE.RIGHT:
-      return new Three.Vector3(1, 0, 0);
+      return state.rotationVector.add(new Three.Vector3(1 * speed, 0, 0));
+    case ACTIONS.MOVE.RESET:
     default:
       return new Three.Vector3(0, 0, 0);
   }
 };
 
-// TODO: Add factory for Three Objects
-
 const CameraReducer = (state = InitialState, action) => {
   switch (action.type) {
     case ACTIONS.CAMERA.ROTATE:
-      // TODO: Make sure this is immutable
-      if (action.rotation === ACTIONS.RESET) {
-        return {
-          rotationVector: mapRotationToVector(action.rotation),
-        }
-      }
-
       return {
-        rotationVector: state.rotationVector.add(mapRotationToVector(action.rotation)),
+        rotationVector: mapRotationVectorFromAction(state, action),
       };
     default:
       return state;
