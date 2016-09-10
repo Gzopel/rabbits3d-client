0cda126672ebd884476326724c6e153da9b4161f
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-10 13:57:25 +0200

    Moved camera configurations to its reducer

--- views/containers/Camera.jsx
+++ views/containers/Camera.jsx
@@ -1,18 +1,9 @@
 import { connect } from 'react-redux';
 import MovingCamera from '../components/MovingCamera';
 
-const THREE = require('three');
-
 const mapStateToProps = (state) => {
   return {
-    config: {
-      fov: 75,
-      aspect: window.innerWidth / window.innerHeight,
-      near: 1,
-      far: 800,
-      position: new THREE.Vector3(400, 0, 400),
-      lookat: state.Camera.rotationVector,
-    },
+    config: state.Camera.config,
   };
 };
 
--- views/reducers/Camera.js
+++ views/reducers/Camera.js
@@ -3,22 +3,28 @@ import ACTIONS from '../actions';
 const Three = require('three');
 
 const InitialState = {
-  rotationVector: new Three.Vector3(0, 0, 0),
+  config: {
+    fov: 75,
+    aspect: window.innerWidth / window.innerHeight,
+    near: 1,
+    far: 800,
+    position: new Three.Vector3(400, 0, 400),
+    lookat: new Three.Vector3(0, 0, 0),
+  },
 };
 
 const mapRotationVectorFromAction = (state, action) => {
   // TODO: Add factory for Three Objects
-
   const speed = action.speed || 1;
   switch (action.rotation) {
     case ACTIONS.MOVE.UP:
-      return state.rotationVector.add(new Three.Vector3(0, 1 * speed, 0));
+      return state.config.lookat.add(new Three.Vector3(0, 1 * speed, 0));
     case ACTIONS.MOVE.DOWN:
-      return state.rotationVector.add(new Three.Vector3(0, -1  * speed, 0));
+      return state.config.lookat.add(new Three.Vector3(0, -1  * speed, 0));
     case ACTIONS.MOVE.LEFT:
-      return state.rotationVector.add(new Three.Vector3(-1  * speed, 0, 0));
+      return state.config.lookat.add(new Three.Vector3(-1  * speed, 0, 0));
     case ACTIONS.MOVE.RIGHT:
-      return state.rotationVector.add(new Three.Vector3(1 * speed, 0, 0));
+      return state.config.lookat.add(new Three.Vector3(1 * speed, 0, 0));
     case ACTIONS.MOVE.RESET:
     default:
       return new Three.Vector3(0, 0, 0);
@@ -29,7 +35,10 @@ const CameraReducer = (state = InitialState, action) => {
   switch (action.type) {
     case ACTIONS.CAMERA.ROTATE:
       return {
-        rotationVector: mapRotationVectorFromAction(state, action),
+        config: {
+          ...InitialState.config,
+          lookat: mapRotationVectorFromAction(state, action),
+        },
       };
     default:
       return state;
