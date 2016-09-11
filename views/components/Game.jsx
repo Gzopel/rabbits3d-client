795ae72e75cfba52da5a67f0436c3259c5db6ec6
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-11 19:11:02 +0100

    Updated game component to listen to resize events

--- views/components/Game.jsx
+++ views/components/Game.jsx
@@ -2,15 +2,46 @@ import React from 'react';
 import GameScene from './GameScene';
 import Map from './Map';
 import Character from './Character';
+import * as BrowserActions from '../actions/Browser';
 
-const Game = () => {
-  return (
-    <GameScene width={window.innerWidth} height={window.innerHeight}>
-      <Map>
-        <Character />
-      </Map>
-    </GameScene>
-  );
+class Game extends React.Component {
+  componentWillMount() {
+    this.resizeGameScene();
+  }
+
+  componentDidMount() {
+    this.props.dispatch(
+      BrowserActions.addEventListener('Game', 'resize', this.resizeGameScene)
+    );
+  }
+
+  resizeGameScene = () => {
+    this.props.dispatch(
+      BrowserActions.updateViewportSize()
+    );
+  };
+
+  render() {
+    if (!this.props.size.width || !this.props.size.height) {
+      return false;
+    }
+
+    return (
+      <GameScene width={this.props.size.width} height={this.props.size.height}>
+        <Map>
+          <Character />
+        </Map>
+      </GameScene>
+    );
+  }
+}
+
+Game.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  size: React.PropTypes.shape({
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+  }),
 };
 
 export default Game;
