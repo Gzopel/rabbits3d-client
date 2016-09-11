69423615a3eab8768aaa695f881a0b6f1565311b
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2016-09-11 19:11:22 +0100

    Use game container instead of component

--- views/index.js
+++ views/index.js
@@ -1,9 +1,11 @@
+if (module.hot) { module.hot.accept(); }
+
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
-import Game from './components/Game';
+import Game from './containers/Game';
 import RootReducer from './reducers';
 import DevTools from './containers/DevTools';
 
