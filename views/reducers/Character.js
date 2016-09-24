49416be3015a21f55399d1b23a33f89efd572ec5
glopez <jepunto@gmail.com>
2016-09-24 15:34:28 -0300

    Character rotation for mouse move.

--- views/reducers/Character.js
+++ views/reducers/Character.js
@@ -6,29 +6,24 @@ const Three = require('three');
 const InitialState = {
   characterPosition: new Three.Vector3(0, 0, 0),
   //walkTarget: new Three.Vector3(0, 0, 0),
-  //characterRotation: new Three.Euler(0,0,0,'XYZ'),
+  characterRotation: new Three.Euler(0,0,0,'XYZ'),
 };
 
 const Character = (state = InitialState, action) => {
   switch (action.type) {
     case ACTIONS.CHARACTER.WALK:
-      /*
-      const point = nextPosition(state.characterPosition, action);
+      const nextPosition = nextPositionToPoint(state.characterPosition, action);
       const fromV = new Three.Vector3(-1, 0, 0);
-      let toV = state.characterPosition.clone().sub(point).normalize();
-      toV.set(-toV.z,0,toV.x);*/
+      const toV = state.characterPosition.clone().sub(nextPosition).normalize();
+      toV.set(-toV.z, 0, toV.x);
       /*
       * OK, so this works, but its a hack. We are doing something wrong here but what?
-      * *//*
+      * */
       const r = new Three.Euler().setFromQuaternion(new Three.Quaternion().setFromUnitVectors(fromV, toV).normalize(),'XYZ');
       return {
-        characterPosition: point,
-        walkTarget: point,
-        characterRotation:r,
-      };*/
-      return {
-        characterPosition: nextPositionToPoint(state.characterPosition, action),
+        characterPosition: nextPosition,
      //   walkTarget: action.point,
+        characterRotation: r,
       };
     default:
       return state;
